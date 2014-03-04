a0606cb096423d6ad053ddb71d8aeae8057efad2
lizhi <zhi.li@jumacc.com>
2014-03-04 13:22:53 +0800

    delete the wrong comments

--- src/bc.js
+++ src/bc.js
@@ -13,7 +13,7 @@
 	See the License for the specific language governing permissions and
 	limitations under the License.
 */
-//(function(){
+(function(){
 
 	var root = this;
 	/**
@@ -1435,7 +1435,7 @@
 		},
   });
   
-//})();
+})();
 
 
 
