4d261d930fec283bf7f25d63baf5d579ab6bd71e
xudChen <346454763@qq.com>
2014-03-10 18:35:04 +0800

    modify bluetooth/tools/BluetoothDetection.java add htc

--- src/android/org/bcsphere/bluetooth/tools/BluetoothDetection.java
+++ src/android/org/bcsphere/bluetooth/tools/BluetoothDetection.java
@@ -36,11 +36,11 @@ public class BluetoothDetection {
 			if (Tools.isSupportSpecificAPI("samsung")) {
 				editor.putString("API", "samsung").commit();
 			}else if (Tools.isSupportSpecificAPI("htc")) {
-				
+				editor.putString("API", "htc").commit();
 			}else if (Tools.isSupportSpecificAPI("xiaomi")){
-				
+				editor.putString("API", "xiaomi").commit();
 			}else if (Tools.isSupportSpecificAPI("motorola")) {
-				
+				editor.putString("API", "motorola").commit();
 			}else {
 				if (Tools.getSupportBasebandVersionBrand() != null) {
 					if (Tools.getSupportBasebandVersionBrand().equals("xiaomi")) {
